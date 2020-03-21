f788015f9d0e6b65042ea2f90d573f9d2eec17c1
Aidan Oakley <oakleyaidan@gmail.com>
2020-03-21 09:15:45 -0700

    removed more unneeded code

--- App.js
+++ App.js
@@ -1,22 +1,6 @@
 import React from "react";
 import MainView from "./MainView";
-import { AsyncStorage } from "react-native";
-import { Notifications } from "expo";
-import * as TaskManager from "expo-task-manager";
 
 export default function App() {
   return <MainView />;
 }
-
-const getData = async () => {
-  try {
-    let value = await AsyncStorage.getItem("locations");
-    if (value !== null) {
-      return value;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.log("error fetching locations", error);
-  }
-};
